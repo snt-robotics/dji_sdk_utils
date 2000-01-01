1491637c1a2bed63686cab6bd3d443abaa096b62
Maciej Zurad <maciej.zurad@gmail.com>
2000-01-01 01:22:05 +0100

    fixing a bug where gimbal publisher didnt compensate for UAV absolute orientation with Zenmuse X3 gimbal

--- dji_gimbal/nodes/gimbal_joints_publisher.py
+++ dji_gimbal/nodes/gimbal_joints_publisher.py
@@ -8,6 +8,7 @@ Therefore, we correct for it by subtracting the yaw to north value
 """
 
 import rospy, math, tf_conversions
+from operator import sub
 from dji_sdk.msg import Gimbal, AttitudeQuaternion
 from std_msgs.msg import Header
 from sensor_msgs.msg import JointState
@@ -16,18 +17,17 @@ NODE_NAME = 'gimbal_joints_publisher'
 JOINTS = ['yaw', 'roll', 'pitch']
 
 baselink_frame_id = None
-subtract_north = None
+subtract_orientation = None
 
-current_yaw_to_north = 0.0
-convert_radians_to_std_range = lambda x: ( x + math.pi) % (2 * math.pi ) - math.pi
+current_abs_orientation = None
+convert_degrees_to_std_range = lambda x: ( x + 180) % 360 - 180
 
 def attitude_quaternion_callback(msg):
   q = (msg.q1, msg.q2, msg.q3, msg.q0)
-  _, _, yaw = tf_conversions.transformations.euler_from_quaternion(q)
-  yaw = convert_radians_to_std_range(yaw)
-  yaw = math.degrees(yaw)
-  global current_yaw_to_north
-  current_yaw_to_north = yaw
+  r, p, y = tf_conversions.transformations.euler_from_quaternion(q)
+  angles = map(convert_degrees_to_std_range, [y, r, p])
+  global current_abs_orientation
+  current_abs_orientation = angles
 
 def gimbal_callback(msg):
 
@@ -35,14 +35,19 @@ def gimbal_callback(msg):
   h.stamp = msg.header.stamp
   h.frame_id = baselink_frame_id
 
-  if not current_yaw_to_north and subtract_north:
+  if not current_abs_orientation and subtract_orientation:
     rospy.logwarn('%s: Did NOT receive ~attitude_quaternion msg, so can\'t corrent to north', NODE_NAME)
     return
+
+  gimbal_positions = [msg.yaw, msg.roll, msg.pitch]
+  if subtract_orientation:
+    gimbal_positions = map(sub, gimbal_positions, current_abs_orientation)
+  gimbal_positions = map(math.radians, gimbal_positions)
 
   joint_states = JointState()
   joint_states.header = h
   joint_states.name = JOINTS
-  joint_states.position = map(math.radians, [msg.yaw - current_yaw_to_north, msg.roll, msg.pitch])
+  joint_states.position = gimbal_positions
   joint_states.velocity  = [0, 0, 0]
   joint_states.effort    = [0, 0, 0]
   gimbal_joint_states_pub.publish(joint_states)
@@ -50,14 +55,14 @@ def gimbal_callback(msg):
 rospy.init_node(NODE_NAME)
 
 baselink_frame_id = rospy.get_param('~baselink_frame_id')
-subtract_north = rospy.get_param('~subtract_north')
+subtract_orientation = rospy.get_param('~subtract_orientation')
 
 rospy.Subscriber('gimbal', Gimbal, gimbal_callback)
 
-if subtract_north: 
+if subtract_orientation: 
   rospy.Subscriber('attitude_quaternion', AttitudeQuaternion, attitude_quaternion_callback)
   rospy.loginfo('Gimbal Yaw will be corrected to base_link by subtracting the angle to North')
 
-gimbal_joint_states_pub = rospy.Publisher('gimbal_joint_states', JointState, queue_size = 100)
+gimbal_joint_states_pub = rospy.Publisher('gimbal_joint_states', JointState, queue_size = 1)
 
 rospy.spin()
