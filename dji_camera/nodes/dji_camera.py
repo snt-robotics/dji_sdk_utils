355cb9da3e45cba2bf2cf39fd46a66a68a13e120
Maciej Zurad <maciej.zurad@gmail.com>
2016-02-11 20:00:50 +0100

    fixing error with frame ids

--- dji_camera/nodes/dji_camera.py
+++ dji_camera/nodes/dji_camera.py
@@ -29,7 +29,13 @@ def waypoint_reached_callback(msg):
     rospy.logwarn("Exception caught while getting transform")
     return
 
+  print source_frame_id, target_frame_id
   print waypoint_reached_time, before_calling_camera, after_calling_camera
+  print trans
+  #output_file.write(waypoint_reached_time.to_sec())
+  #output_file.write(trans.transform.translation.x)
+  #output_file.write(trans.transform.translation.y)
+  #output_file.write(trans.transform.translation.z)
 
 rospy.init_node("dji_camera")
 
@@ -39,6 +45,10 @@ tf_listener = tf2_ros.TransformListener(tf_buffer)
 source_frame_id = rospy.get_param("~source_frame_id")
 target_frame_id = rospy.get_param("~target_frame_id")
 
+
+source_frame_id = "robot_base_link" 
+target_frame_id = "airplane_base_link"
+ 
 output_filename = rospy.get_param("~output_filename")
 
 camera_client = rospy.ServiceProxy('camera_action', CameraAction)
