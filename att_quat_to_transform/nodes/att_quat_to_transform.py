abfa904faa7197658da243e34cbf637daff9cfb2
Maciej Zurad <maciej.zurad@gmail.com>
2017-06-13 16:36:27 +0200

    Attitude quaternion now works, adding lab launch script

--- att_quat_to_transform/nodes/att_quat_to_transform.py
+++ att_quat_to_transform/nodes/att_quat_to_transform.py
@@ -13,34 +13,57 @@ from geometry_msgs.msg import (
   Point
 )
 
+NODE_NAME = 'att_quat_to_transform'
+
 def attitude_quaternion_callback(data):
   '''
   DJI SDK defines quaternion as (w x y z)
   ROS defines quaternion as (x y z w)
   Therefore we have to swap some values
+  Also we have to convert from NED to ENU frame,
+  see the negative sign for y (q2) and z (q3)
+     
+    x                 z  x
+   /                  | /
+  /____y    ==>  y____|/
+  |
+  |z
+
+  NED       ==>   ENU
+
   '''
-  quat = (data.q1, data.q2, data.q3, data.q0)
-  header = data.header
-  header.frame_id = 'm600/rtk_pos'
+
+  time = data.header.stamp
+
+  quat = (data.q1, -data.q2, -data.q3, data.q0)
+  h = Header()
+  h.stamp = time
+  h.frame_id = baselink_translation_frame_id
   t_stamped = TransformStamped()
-  t_stamped.header = header
-  t_stamped.child_frame_id  = 'm600/orientation'
+  t_stamped.header = h
+  t_stamped.child_frame_id = estimated_baselink_frame_id
   t_stamped.transform = Transform()
-  t_stamped.transform.translation = Vector3(1.0, 0.0, 0.0)
+  t_stamped.transform.translation = Vector3(0.0, 0.0, 0.0)
   t_stamped.transform.rotation = Quaternion(*quat)
   tf_broadcaster.sendTransform(t_stamped)
 
   pose_stamped = PoseStamped()
-  pose_stamped.header = header
+  pose_stamped.header = h
   pose_stamped.pose = Pose()
   pose_stamped.pose.position = Point(0, 0, 0)
   pose_stamped.pose.orientation = Quaternion(*quat)
   local_orient_pose_pub.publish(pose_stamped)
 
+rospy.init_node(NODE_NAME)
+rospy.Subscriber('attitude_quaternion', AttitudeQuaternion, attitude_quaternion_callback)
 local_orient_pose_pub = rospy.Publisher('local_orientation_pose', PoseStamped, queue_size = 1)
 local_orient_transform_pub = rospy.Publisher('local_orientation_transform', TransformStamped, queue_size = 1)
 tf_broadcaster = tf2_ros.TransformBroadcaster()
 
-rospy.init_node('att_quat_to_transform')
-rospy.Subscriber('attitude_quaternion', AttitudeQuaternion, attitude_quaternion_callback)
+baselink_translation_frame_id = rospy.get_param('~baselink_translation_frame_id')
+estimated_baselink_frame_id   = rospy.get_param('~estimated_baselink_frame_id')
+
+rospy.loginfo('%s: Assuming baselink translation (no orientation) frame id to be %s', NODE_NAME, baselink_translation_frame_id)
+rospy.loginfo('%s: Assuming estimated baselink frame id to be %s', NODE_NAME, estimated_baselink_frame_id)
+
 rospy.spin()
---
+++ att_quat_to_transform/nodes/strip_orientation_from_pose.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import rospy
+import tf, tf2_ros
+from std_msgs.msg import Header
+from geometry_msgs.msg import (
+  TransformStamped,
+  Transform,
+  PoseStamped,
+  Quaternion
+)
+
+NODE_NAME = 'strip_orientation_from_pose'
+
+def dji_pose_stamped_callback(data):
+  '''
+  Strip orientation from pose stamped  
+  '''
+
+  #time = rospy.Time.now()
+  time = data.header.stamp
+
+  h = Header()
+  h.stamp = time
+  h.frame_id = map_frame_id
+
+  t_stamped = TransformStamped()
+  t_stamped.header = h
+  t_stamped.child_frame_id = baselink_translation_frame_id
+  t_stamped.transform = Transform()
+  t_stamped.transform.translation = data.pose.position 
+  t_stamped.transform.rotation = Quaternion(0, 0, 0, 1) 
+  tf_broadcaster.sendTransform(t_stamped)
+
+rospy.init_node(NODE_NAME)
+rospy.Subscriber('/DJI/pose', PoseStamped, dji_pose_stamped_callback)
+tf_broadcaster = tf2_ros.TransformBroadcaster()
+
+map_frame_id = rospy.get_param('~map_frame_id')
+baselink_translation_frame_id = rospy.get_param('~baselink_translation_frame_id')
+
+rospy.loginfo('%s: Assuming map frame id to be %s', NODE_NAME, map_frame_id)
+rospy.loginfo('%s: Assuming baselink translation (no orientation) frame id to be %s', NODE_NAME, baselink_translation_frame_id)
+
+rospy.spin()
