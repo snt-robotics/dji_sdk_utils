6f9743cc1b0503865b285acc2941f5fdcd24552c
Maciej Zurad <maciej.zurad@gmail.com>
2017-06-28 19:38:07 +0200

    fixing some help strings and cleaning the code

--- dji_rtk_tools/scripts/rtk_rosbag_to_kml.py
+++ dji_rtk_tools/scripts/rtk_rosbag_to_kml.py
@@ -10,8 +10,8 @@ DEFAULT_MSG_INPUT_FREQ = 50.0
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description="ROS Bag RTK extraction and conversion to KML")
 
-  parser.add_argument('output', type=str, help='Output filepath to a ROSBAG containing dji_sdk/A3RTK messages')
-  parser.add_argument('-r', '--rosbag', type=str, help='ROSBAG file containing A3_RTK messages', required=True)
+  parser.add_argument('output', type=str, help='Output filepath to a KML file containing a GPS track')
+  parser.add_argument('-b', '--rosbag', type=str, help='ROSBAG file containing A3_RTK messages', required=True)
   parser.add_argument('-t', '--topic', type=str,  help='A3_RTK topic', default=DEFAULT_RTK_TOPIC)
   parser.add_argument('-f', '--freq', type=float,  help='Frequncy at which we sample messages', default=DEFAULT_MSG_OUTPUT_FREQ)
   args = parser.parse_args()
@@ -28,7 +28,6 @@ if __name__ == '__main__':
 
     t = float('{}.{}'.format(t.secs, t.nsecs))
     t = datetime.fromtimestamp(t).isoformat()
-    t = simplekml.TimeStamp(t)
 
     lat = msg.latitude_RTK
     lon = msg.longitude_RTK
@@ -43,7 +42,7 @@ if __name__ == '__main__':
   bag.close()
 
   kml = simplekml.Kml(name="Tracks", open=1)
-  doc = kml.newdocument(name='GPS device', snippet=simplekml.Snippet('Created Wed Jun 2 15:33:39 2010'))
+  doc = kml.newdocument(name='DJI D-RTK+', snippet=simplekml.Snippet('Created {}'.format(timestamps[-1])))
   doc.lookat.gxtimespan.begin = timestamps[0]
   doc.lookat.gxtimespan.end   = timestamps[1]
   doc.lookat.longitude = coords[0][0]
@@ -51,18 +50,18 @@ if __name__ == '__main__':
   doc.lookat.range     = coords[0][2] + 20
 
   fol = doc.newfolder(name='UAV Track')
-  trk = fol.newgxtrack(name='2010-05-28T01:16:35.000Z')
+  trk = fol.newgxtrack(name=timestamps[-1])
 
   trk.newwhen(timestamps)
   trk.newgxcoord(coords)
 
   trk.stylemap.normalstyle.iconstyle.icon.href = 'http://earth.google.com/images/kml-icons/track-directional/track-0.png'
-  trk.stylemap.normalstyle.linestyle.color = 'ff000000'
+  trk.stylemap.normalstyle.linestyle.color = 'ffff0000'
   trk.stylemap.normalstyle.linestyle.width = 12
   trk.stylemap.highlightstyle.iconstyle.icon.href = 'http://earth.google.com/images/kml-icons/track-directional/track-0.png'
   trk.stylemap.highlightstyle.iconstyle.scale = 1.2
-  trk.stylemap.highlightstyle.linestyle.color = 'ff000000'
+  trk.stylemap.highlightstyle.linestyle.color = 'ffff0000'
   trk.stylemap.highlightstyle.linestyle.width = 16
 
-
   kml.save(args.output)
+
