522fd66a1b5e361b2ce6f2be190031812001dc5b
Maciej Zurad <maciej.zurad>
2017-07-12 17:54:33 +0000

    adding a rosbag record for m600

---
+++ dji_launchers/scripts/rosbag-m600.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -z "${1}" ]]; then
+  echo 'You must specify a directory to which rosbag will be saved!'
+  exit
+fi
+
+echo 'Creating directory ...'
+mkdir -p $1
+
+if [ $? -eq 0 ]; then
+  echo 'Launching rosbag...'
+  rosbag record -b 1024 -o "${1}/" /dji_sdk/A3_RTK /dji_sdk/A3_GPS /dji_sdk/acceleration /dji_sdk/attitude_quaternion /dji_sdk/gimbal /dji_sdk/gimbal_joint_states /dji_sdk/compass  /tf /tf_static /dji_sdk/initial_position
+else
+  echo 'Directory count not be created, do you have permissions ?'
+fi
+
