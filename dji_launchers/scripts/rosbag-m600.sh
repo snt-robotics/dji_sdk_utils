909f466109178c52e359d2999c492936b43166aa
Maciej Zurad <maciej.zurad@gmail.com>
2017-07-13 09:55:08 +0200

    Revert "adding more rosbag record scripts"
    This reverts commit 7c9042a392c05fd65f84614fa0fa77eb91f0a9ad.

--- dji_launchers/scripts/rosbag-m600.sh
+++ dji_launchers/scripts/rosbag-m600.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-source ./record.sh
 
-BUFFER=1024
-TOPICS="/dji_sdk/A3_RTK /dji_sdk/A3_GPS /dji_sdk/acceleration /dji_sdk/attitude_quaternion /dji_sdk/gimbal /dji_sdk/gimbal_joint_states /dji_sdk/compass /tf /tf_static /dji_sdk/initial_position"
+if [[ -z "${1}" ]]; then
+  echo 'You must specify a directory to which rosbag will be saved!'
+  exit
+fi
 
-record_topics $1 "${TOPICS}" "${BUFFER_SIZE}"
+echo 'Creating directory ...'
+mkdir -p $1
+
+if [ $? -eq 0 ]; then
+  echo 'Launching rosbag...'
+  rosbag record -b 1024 -o "${1}/" /dji_sdk/A3_RTK /dji_sdk/A3_GPS /dji_sdk/acceleration /dji_sdk/attitude_quaternion /dji_sdk/gimbal /dji_sdk/gimbal_joint_states /dji_sdk/compass  /tf /tf_static /dji_sdk/initial_position
+else
+  echo 'Directory count not be created, do you have permissions ?'
+fi
+
