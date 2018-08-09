6e4b51b7e9de4fcbd8d34000d13ccf713a4ad048
Maciej Zurad <maciej.zurad@gmail.com>
2018-08-09 20:17:44 +0200

    adding multiple frame pairs support

--- dji_camera/nodes/dji_camera.py
+++ dji_camera/nodes/dji_camera.py
@@ -1,59 +1,124 @@
 #!/usr/bin/env python
+import os
+import sys
+import yaml
+import time
+import csv
+
+import numpy as np
+
 import rospy
+import tf2_ros
 
 from robot_geometry_msgs.msg import RobotState
 from dji_sdk.srv import CameraAction
 
-import tf2_ros
-#
-# camera_action = None
-# source_frame_id = None
-# target_frame_id = None
-#
-# output_filename = None
-#
-# tf_buffer = None
-# tf_listener = None
-#
-# output_file = None
+NODE_NAME = 'dji_camera'
+
+def transform_msg_to_list(msg):
+  if msg is None:
+    return list(np.full((7,), np.nan))
+
+  tran = msg.transform.translation
+  rot  = msg.transform.rotation
+  return [tran.x, tran.y, tran.z, rot.x, rot.y, rot.z, rot.w]
 
 def waypoint_reached_callback(msg):
   waypoint_reached_time = msg.header.stamp
   before_calling_camera = rospy.Time.now()
   camera_client(0);
-  after_calling_camera = rospy.Time.now()
 
-  try:
-    trans = tf_buffer.lookup_transform(source_frame_id, target_frame_id, waypoint_reached_time)
-  except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-    rospy.logwarn("Exception caught while getting transform")
-    return
+  for pair in frame_id_pairs:
 
-  print source_frame_id, target_frame_id
-  print waypoint_reached_time, before_calling_camera, after_calling_camera
-  print trans
-  #output_file.write(waypoint_reached_time.to_sec())
-  #output_file.write(trans.transform.translation.x)
-  #output_file.write(trans.transform.translation.y)
-  #output_file.write(trans.transform.translation.z)
+    source_frame_id = pair['source_frame_id']
+    target_frame_id = pair['target_frame_id']
+    writer = pair['writer']
 
-rospy.init_node("dji_camera")
+    rospy.loginfo(('{}: Looking for transform '
+                   'from {} to {} at time {:.3f}'
+                   ).format(NODE_NAME,
+                            source_frame_id,
+                            target_frame_id,
+                            waypoint_reached_time.to_sec()))
+
+    transform = None
+    try:
+      transform = tf_buffer.lookup_transform(source_frame_id,
+                                             target_frame_id,
+                                             waypoint_reached_time)
+
+    except (tf2_ros.LookupException,
+            tf2_ros.ConnectivityException,
+            tf2_ros.ExtrapolationException) as e:
+      rospy.logwarn(e)
+
+    row = [
+      counter,
+      waypoint_reached_time.to_sec(),
+      before_calling_camera.to_sec()
+    ] + transform_msg_to_list(transform)
+    writer.writerow(dict(zip(fieldnames, row)))
+
+
+  global counter
+  counter += 1
+
+
+rospy.init_node(NODE_NAME)
 
 tf_buffer = tf2_ros.Buffer()
 tf_listener = tf2_ros.TransformListener(tf_buffer)
 
-source_frame_id = rospy.get_param("~source_frame_id")
-target_frame_id = rospy.get_param("~target_frame_id")
+frame_id_pairs = None
+frame_id_pairs_file = rospy.get_param("~frame_id_pairs")
+with open(frame_id_pairs_file, 'r') as stream:
+  try:
+    frame_id_pairs = yaml.load(stream)
+  except yaml.YAMLError as exc:
+    rospy.loginfo(e)
+    sys.exit(os.EX_DATAERR)
 
+assert len(frame_id_pairs) > 0
 
-source_frame_id = "robot_base_link" 
-target_frame_id = "airplane_base_link"
- 
-output_filename = rospy.get_param("~output_filename")
+output_directory = rospy.get_param("~output_directory")
+output_directory = os.path.join(output_directory, time.strftime('%Y%m%d-%H%M%S'))
+rospy.loginfo('{}: Output directory is: {}'.format(NODE_NAME, output_directory))
 
+if not os.path.exists(output_directory):
+  os.makedirs(output_directory)
+
+fieldnames = [
+  'counter',
+  'waypoint_stamp',
+  'before_pic_stamp',
+  'tx', 'ty', 'tz',
+  'qx', 'qy', 'qz', 'qw'
+]
+rospy.loginfo(('{}: Loaded the following frame id pairs:'.format(NODE_NAME)))
+for pair in frame_id_pairs:
+  source_frame_id = pair['source_frame_id']
+  target_frame_id = pair['target_frame_id']
+  rospy.loginfo(('{}: Frame id pair is {} -> {}'
+                ).format(NODE_NAME, source_frame_id, target_frame_id))
+
+  filename = '{}-to-{}.csv'.format(source_frame_id, target_frame_id)
+  filepath = os.path.join(output_directory, filename)
+
+  f = open(filepath, 'w')
+  writer = csv.DictWriter(f, fieldnames=fieldnames)
+  writer.writeheader()
+
+  pair['file'] = f
+  pair['writer'] = writer
+
+counter = 0
+
+rospy.wait_for_service('camera_action')
 camera_client = rospy.ServiceProxy('camera_action', CameraAction)
 
 rospy.Subscriber('waypoint_reached', RobotState, waypoint_reached_callback)
 
-with open(output_filename, 'wt') as output_file:
-  rospy.spin()
+rospy.spin()
+
+for pair in frame_id_pairs:
+  pair['file'].close()
