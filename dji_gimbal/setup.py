b337c11258321ea667283d01546448515c39b8c7
Maciej Zurad <maciej.zurad@gmail.com>
2018-08-24 13:57:16 +0000

    removing dji_sdk dependency on dji_gimbal

--- dji_gimbal/setup.py
+++ dji_gimbal/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
   packages=['dji_gimbal'],
   package_dir={'': 'src'},
-  requires=['std_msgs', 'rospy', 'sensor_msgs', 'geometry_msgs', 'dji_sdk']
+  requires=['std_msgs', 'rospy', 'sensor_msgs', 'geometry_msgs']
 )
 
 setup(**setup_args)
